8956f2d0bfef866535070d9e9aacccfc888c05b9
sgifch <mivachi@inbox.ru>
2025-11-03 16:58:47 +0400

    Update app.js

--- app.js
+++ app.js
@@ -8,24 +8,28 @@ let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
 
-btn1.addEventListener("click", function() {
+btn1.addEventListener("click", function(event) {
+    event.preventDefault();
     item = "1";
     tg.sendData(item);
     console.log("Запрос 1")
 });
 
-btn2.addEventListener("click", function() {
+btn2.addEventListener("click", function(event) {
+    event.preventDefault();
     item = "2";
     tg.sendData(item);
     console.log("2")
 });
 
-btn3.addEventListener("click", function() {
+btn3.addEventListener("click", function(event) {
+    event.preventDefault();
     item = "3";
     tg.sendData(item);
 });
 
-btn4.addEventListener("click", function() {
+btn4.addEventListener("click", function(event) {
+    event.preventDefault();
     item = "4";
     tg.sendData(item);
 });
